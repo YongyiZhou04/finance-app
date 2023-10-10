8d22af97db15e1a101a33fa6d4a16a226377e773
gy180 <142842008+gy180@users.noreply.github.com>
2023-10-10 03:25:25 +0000

    test

---
+++ main.js
@@ -0,0 +1,28 @@
+// Add your JavaScript code here
+document.getElementById("addTransaction").addEventListener("click", () => {
+    const description = document.getElementById("description").value;
+    const amount = parseFloat(document.getElementById("amount").value);
+
+    if (description && !isNaN(amount)) {
+        addTransaction(description, amount);
+        clearInputFields();
+    }
+});
+
+function addTransaction(description, amount) {
+    const transactionList = document.getElementById("transactionList");
+    const listItem = document.createElement("li");
+    listItem.textContent = `${description}: $${amount.toFixed(2)}`;
+    transactionList.appendChild(listItem);
+
+    // Update the account balance
+    const balanceAmount = document.getElementById("balanceAmount");
+    const currentBalance = parseFloat(balanceAmount.textContent.replace("$", ""));
+    const newBalance = currentBalance + amount;
+    balanceAmount.textContent = `$${newBalance.toFixed(2)}`;
+}
+
+function clearInputFields() {
+    document.getElementById("description").value = "";
+    document.getElementById("amount").value = "";
+}
