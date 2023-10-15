dfa60f7c42793156cdca3c3597d842bb6b1cf597
gy180 <142842008+gy180@users.noreply.github.com>
2023-10-15 17:15:36 +0000

    delete button functional

--- main/main.js
+++ main/main.js
@@ -63,10 +63,7 @@ function switchTab(accountType) {
 
     // Add the "selected-tab" class to the clicked tab button
     document.getElementById(accountType.toLowerCase()).style.backgroundColor = "#6AA7AA";
-
-    
 }
-
 
 // Function to add a transaction
 function addTransaction() {
@@ -181,4 +178,72 @@ function clearInputFields() {
     document.getElementById("category").value = "";
     document.getElementById("amountReceive").value = "";
     document.getElementById("amountSpent").value = "";
+}
+
+// Variable to keep track of whether removal mode is active
+let removeMode = false;
+
+// Variable to keep track of the total balance
+let totalBalance = 0;
+
+// Function to handle row selection
+function toggleRowSelection(event) {
+    if (removeMode) {
+        // In removal mode
+        const row = event.target.parentElement;
+        if (row.classList.contains("selected")) {
+            row.classList.remove("selected");
+            // Update total balance when a row is deselected
+            const balanceCell = row.querySelector(".balance-cell");
+            const balance = parseFloat(balanceCell.textContent);
+            totalBalance += balance;
+        } else {
+            row.classList.add("selected");
+            // Update total balance when a row is selected
+            const balanceCell = row.querySelector(".balance-cell");
+            const balance = parseFloat(balanceCell.textContent);
+            totalBalance -= balance;
+        }
+    }
+}
+
+// Add a click event listener to the "Remove" button
+document.getElementById("remove").addEventListener("click", function () {
+    if (removeMode) {
+        // In removal mode, remove selected rows and update balance
+        const selectedRows = document.querySelectorAll(".transactionList .selected");
+        for (const row of selectedRows) {
+            row.remove();
+        }
+
+        // Update the displayed balance amount
+        const balanceAmount = document.getElementById("balanceAmount");
+        balanceAmount.textContent = `$${totalBalance.toFixed(2)}`;
+
+        // Exit removal mode and reset the button
+        removeMode = false;
+        this.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" fill="none">
+                <rect x="6.67188" y="12.231" width="8.14276" height="26.464" rx="4.07138" transform="rotate(-45 6.67188 12.231)" fill="#C98989"/>
+                <rect x="12.4297" y="30.9438" width="8.14276" height="26.464" rx="4.07138" transform="rotate(-135 12.4297 30.9438)" fill="#C98989"/>
+            </svg>
+            <p>Remove</p>
+        `;
+    } else {
+        // Enter removal mode
+        removeMode = true;
+        this.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" fill="none">
+                <rect x="6.67188" y="12.231" width="8.14276" height="26.464" rx="4.07138" transform="rotate(-45 6.67188 12.231)" fill="red"/>
+                <rect x="12.4297" y="30.9438" width="8.14276" height="26.464" rx="4.07138" transform="rotate(-135 12.4297 30.9438)" fill="red"/>
+            </svg>
+            <p>Delete</p>
+        `;
+    }
+});
+
+// Add a click event listener to the tables to toggle row selection
+const transactionLists = document.querySelectorAll(".transactionList");
+for (const transactionList of transactionLists) {
+    transactionList.addEventListener("click", toggleRowSelection);
 }
