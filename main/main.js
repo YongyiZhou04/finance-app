bf25994db1067b68be971fa926dfb506e0bbacf3
gy180 <142842008+gy180@users.noreply.github.com>
2023-10-15 03:08:20 +0000

    enter functionality + increase usability

--- main/main.js
+++ main/main.js
@@ -1,4 +1,5 @@
-document.getElementById("addTransaction").addEventListener("click", () => {
+// Function to add a transaction
+function addTransaction() {
     // Get the input values
     const date = document.getElementById("date").value;
     const description = document.getElementById("description").value;
@@ -7,45 +8,49 @@ document.getElementById("addTransaction").addEventListener("click", () => {
     const spend = parseFloat(document.getElementById("amountSpend").value);
 
     // Check if required fields are filled
-    if (date && description && transfer && (!isNaN(receive) || !isNaN(spend)) ) {
+    if (date && description && transfer && (!isNaN(receive) || !isNaN(spend))) {
         // Create a new row in the table
         const transactionList = document.getElementById("transactionList");
-        const newRow = transactionList.insertRow(-1); 
+        const newRow = transactionList.insertRow(-1);
 
         // Add data to the new row
         const dateCell = newRow.insertCell(0);
         dateCell.textContent = date;
         if (transactionList.rows.length % 2 === 1) {
-            dateCell.style.backgroundColor = "#CCE0DC";}
+            dateCell.style.backgroundColor = "#CCE0DC";
+        }
 
         const descriptionCell = newRow.insertCell(1);
         descriptionCell.textContent = description;
         if (transactionList.rows.length % 2 === 1) {
-            descriptionCell.style.backgroundColor = "#CCE0DC";}
+            descriptionCell.style.backgroundColor = "#CCE0DC";
+        }
 
         const transferCell = newRow.insertCell(2);
         transferCell.textContent = transfer;
         if (transactionList.rows.length % 2 === 1) {
-            transferCell.style.backgroundColor = "#CCE0DC";}
+            transferCell.style.backgroundColor = "#CCE0DC";
+        }
 
         const receiveCell = newRow.insertCell(3);
         var newRec = 0.0;
-        if(!(isNaN(receive))){
+        if (!(isNaN(receive))) {
             newRec = receive;
         }
         receiveCell.textContent = newRec.toFixed(2);
         if (transactionList.rows.length % 2 === 1) {
-            receiveCell.style.backgroundColor = "#CCE0DC";}
+            receiveCell.style.backgroundColor = "#CCE0DC";
+        }
 
         var newSpend = 0.0;
         const spendCell = newRow.insertCell(4);
-        if(!(isNaN(spend))){
+        if (!(isNaN(spend))) {
             newSpend = spend;
         }
         spendCell.textContent = newSpend.toFixed(2);
         if (transactionList.rows.length % 2 === 1) {
-            spendCell.style.backgroundColor = "#CCE0DC";}
-        
+            spendCell.style.backgroundColor = "#CCE0DC";
+        }
 
         // Calculate and update the new balance
         const balanceAmount = document.getElementById("balanceAmount");
@@ -56,15 +61,50 @@ document.getElementById("addTransaction").addEventListener("click", () => {
         const balanceCell = newRow.insertCell(5);
         balanceCell.textContent = newBalance.toFixed(2);
         if (transactionList.rows.length % 2 === 1) {
-            balanceCell.style.backgroundColor = "#CCE0DC";}
+            balanceCell.style.backgroundColor = "#CCE0DC";
+        }
 
         // Clear the input fields
         clearInputFields();
+
+        // // Sort the table by date after adding a new transaction
+        // sortTableByDate();
     } else {
         alert("Please fill in all required fields with valid values. Required fields include Date, Description, Transfer, and either receive or spend.");
     }
+}
+
+// Event listener for clicking the "addTransaction" button
+document.getElementById("addTransaction").addEventListener("click", addTransaction);
+
+// Event listeners for Enter key press in input fields
+document.getElementById("date").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addTransaction();
+    }
+});
+document.getElementById("description").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addTransaction();
+    }
+});
+document.getElementById("category").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addTransaction();
+    }
+});
+document.getElementById("amountReceive").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addTransaction();
+    }
+});
+document.getElementById("amountSpend").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addTransaction();
+    }
 });
 
+// Function to clear input fields
 function clearInputFields() {
     document.getElementById("date").value = "";
     document.getElementById("description").value = "";
@@ -73,6 +113,3 @@ function clearInputFields() {
     document.getElementById("amountSpend").value = "";
     document.getElementById("balance").value = "";
 }
-
-
-// https://codepen.io/demartini/pen/EJvgPa
